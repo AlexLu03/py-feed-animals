4e8442f822cc9a86beb1ecadb45276f6ced093bb
Alex_Lushenko <mralealex9@gmail.com>
2025-07-30 21:21:53 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class Animal:
-    def __init__(self, name: str, appetite: int, is_hungry: bool = True):
+    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
@@ -19,7 +19,7 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self, name: str, is_hungry: bool = True):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=3, is_hungry=is_hungry)
 
     def catch_mouse(self) -> None:
